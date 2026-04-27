e0f368760153c747002c0d75b473dfc6c821b7ac
KlekotkaOrigen <fudomyosama@hotmail.com>
2026-04-27 16:06:42 -0300

    header first version

--- includes/header.php
+++ includes/header.php
@@ -2,42 +2,41 @@
 <html lang="es">
 
 <head>
-<!-- Caracteres con los que se va a trabajar -->
 <meta charset="UTF-8"> 
-<!-- En la ventana acomodar el contenido al ancho de la misma y con el zoom 1:1 -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Optimización para SEO -->
 <meta name="description" content="Clínica odontológica OdontoPlus">
 <meta name="keywords" content="odontologia, clinica dental, implantes">
-<!-- Autores -->
 <meta name="author" content="Damian Dominguez & Cortinez Matias">
-
-<!-- Título en la pestaña de la ventana o la ventana -->
 <title>OdontoWeb</title>
-
-<!-- Ya lo dejo para cuando haga el archivo del CSS -->
-<link rel="stylesheet" href="css/style.css">
-
+<link rel="icon" href="./img/logo.png">
+<link rel="stylesheet" href="css/head_style.css">
 </head>
+
 
 <body>
 
 <header>
+
 <!-- Logo -->
-<div class="logo">
-<img src="/img/logo.png" alt="Logo Empresa">
-<h1>OdontoPlus</h1>
-</div>
+<a href="index.php">
+    <img src="./img/logo.png" alt="Logo Empresa" style="width: 50px;height:50px;" id="logo">
+</a>
+
+<h1>ODONTO PLUS</h1>
 
 <!-- Lista de opciones para navegar rapidamente en la página, busca archivos en la carpeta del proyecto/web o id en la misma página (#xx === idxx) -->
 <nav>
 <ul>
-<li><a href="index.php">Inicio</a></li>
-<li><a href="#nosotros">Nosotros</a></li>
-<li><a href="#servicios">Servicios</a></li>
-<li><a href="#contacto">Contacto</a></li>
-<li><a href="login.php">Login</a></li>
+<li><a href="index.php">INICIO</a></li>
+<li><a href="#nosotros">NOSOTROS</a></li>
+<li><a href="#servicios">SERVICIOS</a></li>
+<li><a href="#contacto">CONTACTO</a></li>
+<li><a href="login.php">LOGIN</a></li>
 </ul>
 </nav>
+
+<a href="turno.php">
+    <button id="boton">RESERVE SU TURNO</button>
+</a>
 
 </header>
